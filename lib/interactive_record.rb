2c92e921e9761356871ca2afa13e37217a2228f8
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-17 23:23:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,16 +20,16 @@ class InteractiveRecord
     column_names.compact
   end
 
-  def initialize(options = {})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
-
-  # def initialize(options={})
+  # def initialize(options = {})
   #   options.each do |property, value|
   #     self.send("#{property}=", value)
   #   end
   # end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
 
 end
