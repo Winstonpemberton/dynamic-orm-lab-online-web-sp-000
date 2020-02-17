eb648436589df0eb586bd649f9b75bb2408844dd
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-17 23:14:48 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,15 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
 end
