e3a94a17c32bb2e660b1488bc6c5ec1d49de322e
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-17 23:29:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,12 +18,14 @@ class InteractiveRecord
       column_names << row["name"]
     end
     column_names.compact
+
+      self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
   end
 
 
-    self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
+
 
   def initialize(options = {})
     options.each do |property, value|
